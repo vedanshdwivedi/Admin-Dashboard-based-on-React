3838f161d41fdd6bea381718445d5569aad47a57
Vedansh Dwivedi <vedansh.devofficial@gmail.com>
2022-07-22 20:25:51 +0000

    Created Navbar

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import TopBar from './components/topbar/TopBar';
+
 function App() {
-	return <div className="App">Hello</div>;
+	return (
+		<div className="App">
+			<TopBar />
+		</div>
+	);
 }
 
 export default App;
--- dashboard/src/components/topbar/TopBar.js
+++ dashboard/src/components/topbar/TopBar.js
@@ -1,6 +1,36 @@
 import React from 'react';
-
+import './topbar.css';
 
 export default function TopBar() {
-	return <div>Top Navbar</div>;
+	const navbarStyle = {
+		'z-index': 999,
+	};
+	return (
+		<>
+			<nav className="navbar fixed-top navbar-dark bg-dark text-light" style={navbarStyle}>
+				<div className="container-fluid">
+					<a className="navbar-brand">Dashboard</a>
+					<div className="d-flex iconContainer">
+						<div className="navbar-icons">
+							<i className="fa-regular fa-bell"></i>
+							<span className="notification-badge"> 2</span>
+						</div>
+						<div className="navbar-icons">
+							<i className="fa-solid fa-globe"></i>
+							<span className="notification-badge"> 5</span>
+						</div>
+						<div className="navbar-icons">
+							<i className="fa-solid fa-gear"></i>
+						</div>
+						<div className="navbar-icons"></div>
+						<img
+							className="topAvatar"
+							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+							alt=""
+						/>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 }
