1c08b409d3509aa9e07901d9a58499f06feb31c0
Vedansh Dwivedi <vedansh.devofficial@gmail.com>
2022-07-23 20:01:36 +0000

    Added Featured info for homepage content

---
+++ dashboard/src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.css';
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+
+export default function Home() {
+	return (
+		<>
+			<div className="home"><FeaturedInfo /></div>
+		</>
+	);
+}
