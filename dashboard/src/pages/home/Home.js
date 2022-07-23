c9f093d4ac07c90c307c54da27912ab6e40de2fd
Vedansh Dwivedi <vedansh.devofficial@gmail.com>
2022-07-23 20:49:13 +0000

    Integrated Recharts

--- dashboard/src/pages/home/Home.js
+++ dashboard/src/pages/home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './home.css';
-import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import Chart from '../../components/charts/Chart';
+import {UserData} from '../../DummyData'
 
 export default function Home() {
 	return (
 		<>
-			<div className="home"><FeaturedInfo /></div>
+			<div className="home">
+				<FeaturedInfo />
+				<Chart data={UserData} title="User Analytics" line_key="User Count" x_name="name" />
+			</div>
 		</>
 	);
 }
