c70a3aefa27ecb349ab71a0dbd2d9ce9589a2bc4
Vedansh Dwivedi <vedansh.devofficial@gmail.com>
2022-07-24 18:59:26 +0000

    Added Files

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import TopBar from './components/topbar/TopBar';
 import SideBar from './components/sidebar/Sidebar';
 import Home from './pages/home/Home';
+import Users from './pages/users/users';
 import './app.css';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
@@ -10,7 +13,12 @@ function App() {
 			<TopBar />
 			<div className="PageContainer">
 				<SideBar />
-				<Home />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/users" element={<Users />} />
+					</Routes>
+				</BrowserRouter>
 			</div>
 		</div>
 	);
